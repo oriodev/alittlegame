68f456fbc75e3a14aee032fbd4c18c5115c314bb
ori <93719767+oriodev@users.noreply.github.com>
2024-03-20 23:28:46 +0000

    basic carousel

---
+++ components/DemonCarousel.tsx
@@ -0,0 +1,31 @@
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/carousel';
+
+import Image from 'next/image';
+import { Card, CardContent } from './ui/card';
+import { DemonCarouselFrame } from './DemonCarouselFrame';
+
+export const DemonCarousel = () => {
+  return (
+    <div className="">
+      <Carousel className="w-full max-w-xs">
+        <CarouselContent>
+          {Array.from({ length: 8 }).map((_, index) => (
+            <CarouselItem key={index}>
+              <div className="p-1 h-full">
+                <DemonCarouselFrame num={index + 1} width={150} />
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </div>
+  );
+};
